84bd967ac8060985d56bad0121d71fdec35a47dd
Rahul Chopra <adrollsai@gmail.com>
2025-12-24 17:48:43 +0530

    kslfjlskf

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 # --- CONFIGURATION ---
 # Using Flash model for low latency
-MODEL = "gemini-2.0-flash-exp"
+MODEL = os.environ.get("MODEL") || "gemini-2.5-flash-native-audio-preview-12-2025"
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 if not GOOGLE_API_KEY:
